afbffcbe83f2261f275e46e47b60be0e20c916d7
Samuel Lord <sam@sllord.info>
2022-07-28 09:52:33 -0700

    Continue on the "All the small things" update
    Features introduced:
    * sleepThenBackToPlayground (wip)
    * Set test game scale to 12

--- 6.py
+++ 6.py
@@ -14,9 +14,17 @@ currentLand.currentLandModels[zones[zID]].reparentTo(render)
 currentLand.currentLandModels[zones[zID]].setHpr(180,0,0)
 currentLand.currentLandModels[zones[zID]].setPos(0,0,0)
 base.localAvatar.setPos(0,0,0)
-currentLand.currentLandModels[zones[zID]].setScale(1)
+currentLand.currentLandModels[zones[zID]].setScale(12)
 
 G["music"].stop()
 G["music"] = loader.loadSfx('phase_4/audio/bgm/EE_DiesandPies.ogg')
 G["music"].setLoop(True)
 G["music"].play()
+
+def sleepThenBackToPlayground():
+    import time
+    time.sleep(43)
+    loadZone(1)
+
+pgthr = threading.Thread(target=sleepThenBackToPlayground, args=(), kwargs={})
+pgthr.start()
